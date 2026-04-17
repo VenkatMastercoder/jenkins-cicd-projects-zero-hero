1398d88daa8ec635d82e3189e6017843b37aee08
VENKATESAN G <96653425+VenkatMastercoder@users.noreply.github.com>
2026-04-17 21:41:33 +0530

    Fix project name and welcome message in API response

--- project-1-freestyle/index.js
+++ project-1-freestyle/index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "ok",
-    project: "project-1-freestyles",
-    message: "Welcome to Project 1 API",
+    project: "project-1-freestyle",
+    message: "Welcome to Project 1 APIs",
   });
 });
 
